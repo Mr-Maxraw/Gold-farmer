b8841a0689caebf97905f80ce16bd5d58e0c69c8
Петухов Максим <maxraw111@gmail.com>
2020-07-30 22:34:13 +0500

    Script for 50/50 game

---
+++ 50_50_game.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import os
+
+driver = webdriver.Chrome()
+driver.get("https://web.simple-mmo.com/")
+element = driver.find_element_by_name('email')
+element.send_keys(os.environ['EMAIL'])
+element = driver.find_element_by_name('password')
+element.send_keys(os.environ['PASSWORD'])
+element.submit()
+
+driver.get('https://web.simple-mmo.com/gamecentre/5050')
+
+wager = ['125', '250', '500', '1000', '2000', '4000', '8000', '16000', '32000', '64000', '128000']
+wager_it = 0
+
+element = driver.find_element_by_name('GoldAmount')
+element.send_keys(wager[wager_it])
+element.submit()
+
+while True:
+    time.sleep(1)
+    element = driver.find_element_by_name('GoldAmount')
+    try:
+        notify = driver.find_element_by_xpath('//div[@class="notice notice-success"]')
+        wager_it = 0
+    except Exception:
+        wager_it = 0 if wager_it == len(wager) - 1 else wager_it + 1 
+    finally:
+        print(wager[wager_it])
+        element.send_keys(wager[wager_it])
+        element.submit()
+        
