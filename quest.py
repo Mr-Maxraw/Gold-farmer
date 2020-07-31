4af702e2d40f843d0fe5c48cb99be94ed98bbcf3
Петухов Максим <maxraw111@gmail.com>
2020-07-31 09:28:33 +0500

    More stable questing

--- quest.py
+++ quest.py
@@ -15,11 +15,14 @@ def do_quests(driver):
     driver.get('https://web.simple-mmo.com/quests/viewall')
 
     while True:
-        open_modal = driver.find_element_by_xpath('//button[text()[contains(., "View Quest")]]')
-        ActionChains(driver).click(open_modal).perform()
-        time.sleep(1)
-        st_q_btn = driver.find_element_by_xpath('//button[text()[contains(., "Perform quest")]]')
-        ActionChains(driver).click(st_q_btn).perform()
+        try:
+            open_modal = driver.find_element_by_xpath('//button[text()[contains(., "View Quest")]]')
+            ActionChains(driver).click(open_modal).perform()
+            time.sleep(1)
+            st_q_btn = driver.find_element_by_xpath('//button[text()[contains(., "Perform quest")]]')
+            ActionChains(driver).click(st_q_btn).perform()
+        except:
+            driver.refresh()
         try:
             time.sleep(1)
             error_msg = driver.find_element_by_id('swal2-validation-message')
