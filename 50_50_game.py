c4019ae10ac6ec74dcd3962533958190af2d342f
Петухов Максим <maxraw111@gmail.com>
2020-08-01 09:38:47 +0500

    Optimize gold gen/logging on it

--- 50_50_game.py
+++ 50_50_game.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from time import sleep
+from time import sleep, strftime, localtime
 import os
 from random import random
 from dotenv import load_dotenv
+import sys
 
 load_dotenv()
 
@@ -17,24 +18,31 @@ element.submit()
 
 driver.get('https://web.simple-mmo.com/gamecentre/5050')
 
-wager = ['125', '250', '500', '1000', '2000', '4000', '8000', '16000', '32000', '64000', '128000']
+wager = ['225', '450', '900', '1800', '3600', '7200', '14400', '28800', '57600', '115200', '230400']
+
 wager_it = 0
 
 element = driver.find_element_by_name('GoldAmount')
 element.send_keys(wager[wager_it])
 element.submit()
 
+sys.stdout = open('gold.log', 'a')
+print(strftime("%Y-%m-%d %H:%M:%S", localtime()))
+print('start gold gen', flush=True)
+
 while True:
-    sleep(1)
+    #sleep(0.5)
     element = driver.find_element_by_name('GoldAmount')
     try:
         notify = driver.find_element_by_xpath('//div[@class="notice notice-success"]')
         wager_it = 0
     except Exception:
-        wager_it = 0 if wager_it == len(wager) - 1 else wager_it + 1 
+        wager_it = 0 if wager_it == len(wager) - 1 else wager_it + 1
+        if wager_it == 0:
+            print('loss')
     finally:
-        sleep(2 * random())
-        print(wager[wager_it])
+        sleep(1.5 * random())
+        print(wager[wager_it], flush=True)
         element.send_keys(wager[wager_it])
         element.submit()
         
